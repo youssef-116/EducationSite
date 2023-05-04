b645cf0e34455bbe594005580a1c0719a3ea40b0
youssef-116 <125500766+youssef-116@users.noreply.github.com>
2023-05-04 23:41:00 +0300

    Update logout.php

--- home/logout.php
+++ home/logout.php
@@ -4,5 +4,5 @@ $conn = mysqli_connect("localhost", "root", "", "education");
 if (!$conn) {die("Connection failed: " . mysqli_connect_error());}
 session_unset();
 session_destroy();
-header('location:../user.php ');
+header('location:../index.php ');
 ?>
