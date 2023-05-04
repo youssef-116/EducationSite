ce4fa1d19d9dc4e20a8a2c4b539888ed079d2305
youssef-116 <125500766+youssef-116@users.noreply.github.com>
2023-05-04 23:28:35 +0300

    Add files via upload

---
+++ user.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User Page</title>
+    <link rel="stylesheet" href="fpage.css">
+</head>
+<body>
+    <div class="container">
+        <div class="content">
+            <h2>hi,<span>user</span></h2>
+            <h1>welcome</h1>
+            <p>This is your knowledge club</p>
+            <a href="login/index.php" class="btn btn1">Professor</a>
+            <a href="login/index2.php" class="btn btn2">Student</a>
+            
+        </div>
+    </div>
+</body>
+</html>
+
